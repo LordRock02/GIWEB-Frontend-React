69450d8544b41f527c34013f50d20adeabf95cec
Roger Gómez Torres <rogergt02@gmail.com>
2024-02-26 17:06:43 -0500

    hello world

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <img src="https://img1.picmix.com/output/stamp/normal/7/4/3/1/2271347_21be6.png" alt="" />
+    </div>
+    
+  )
+}
+
+export default App
